11f43771369ae24e682f681d6997011478da2361
陈豪 <547884940@qq.com>
2014-11-16 04:17:33 +0800

    [all]修改app组织方式

---
+++ PiBox/App/Camera/django/api_urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from PiApp.views import *
+from PiApp.api import *
+
+#/API/application
+
+urlpatterns = patterns('',
+
+)
